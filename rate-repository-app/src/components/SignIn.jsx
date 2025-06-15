1ca091cf2a9032c3d8bdcc73408998c4a04136d9
Alejandro Diaz Crivelli <alejandrodiazcrivelli@gmail.com>
2025-06-15 12:53:41 -0300

    exercise 10.8

--- rate-repository-app/src/components/SignIn.jsx
+++ rate-repository-app/src/components/SignIn.jsx
@@ -1,7 +1,77 @@
 import Text from './Text';
+import { StyleSheet, Pressable, TextInput, View } from 'react-native';
+import { useFormik } from 'formik';
+import theme from '../theme';
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    backgroundColor: "white",
+    marginBottom: 10, 
+    marginHorizontal: 12, 
+    padding: 5,
+    justifyContent: "auto",
+  },
+  child: {
+    flexGrow: 1,
+    margin: 8,
+    padding: 10,
+    alignItems: "center",
+    borderRadius: 10,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "grey",
+  },
+  button: {
+    backgroundColor: theme.colors.primary,
+    color: theme.colors.textTertiary,
+  },
+  buttonText: {
+    color: theme.colors.textTertiary,
+    fontWeight: "bold",
+  }
+});
+
+const initialValues = {
+  username: '',
+  password: '',
+};
+
+const onSubmit = (values) => {
+  console.log(values);
+};
 
 const SignIn = () => {
-  return <Text>The sign-in view</Text>;
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+  });
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        placeholder="Username"
+        value={formik.values.username}
+        onChangeText={formik.handleChange('username')}
+        style={{ ...styles.input, ...styles.child }}
+      />
+      <TextInput
+        placeholder="Password"
+        value={formik.values.password}
+        onChangeText={formik.handleChange('password')}
+        style={{ ...styles.input, ...styles.child }}
+        secureTextEntry
+      />
+      <Pressable 
+        onPress={formik.handleSubmit}
+        style={{ ...styles.button, ...styles.child}}
+      >
+        <Text style={styles.buttonText}>Sign in</Text>
+      </Pressable>
+    </View>
+  );
 };
 
 export default SignIn;
