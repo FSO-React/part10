d2fe4074173ebea957443280ca1305eeaa5c6cce
Alejandro Diaz Crivelli <alejandrodiazcrivelli@gmail.com>
2025-06-17 01:03:19 -0300

    auth context and 'authenticated' apolloClient

--- rate-repository-app/src/hooks/useSignIn.js
+++ rate-repository-app/src/hooks/useSignIn.js
@@ -1,7 +1,9 @@
 import { useMutation } from '@apollo/client';
 import { SIGN_IN } from '../graphql/mutations';
+import useAuthStorage from '../hooks/useAuthStorage';
 
 const useSignIn = () => {
+  const authStorage = useAuthStorage();
   const [mutate, result] = useMutation(SIGN_IN);
   
   const signIn = async ({ username, password }) => {
@@ -9,7 +11,7 @@ const useSignIn = () => {
       variables: { 
         credentials: { username, password }
       }
-    });
+    });    
     return result;
   };
 
